8d78de5a41034125a10ab60d2adb71cb3d8cf76b
Fabio-Bianco <biancobfabio@gmail.com>
2025-05-23 13:03:09 +0200

    feat(server): create entry point and start server on defined port

---
+++ server.js
@@ -0,0 +1,11 @@
+
+// Importa l'app configurata da app.js
+const app = require('./app');
+
+// Porta di ascolto 
+const PORT = process.env.PORT || 3000;
+
+// Avvio server
+app.listen(PORT, () => {
+  console.log(`🪖🐍 Sergente-Serpente attivo sulla porta ${PORT}`);
+});
