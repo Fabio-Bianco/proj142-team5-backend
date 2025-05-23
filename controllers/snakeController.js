fd56db4e352748368bb9fb65eebd3f24282ce9b0
defendimattia <defendi.mattia92@gmail.com>
2025-05-23 16:17:08 +0200

    feat: mysql, db.js

--- controllers/snakeController.js
+++ controllers/snakeController.js
@@ -1,27 +1,27 @@
 //CRUD
 //index
-function index (req,res) {
+function index(req, res) {
     return res.json({
         Message: 'sei su index'
     })
 };
 //show
-function show (req,res) {
+function show(req, res) {
     return res.json({
         Message: 'sei su show'
     })
 };
 //store
-function store (req,res) {
-    return res.json ({ 
+function store(req, res) {
+    return res.json({
         Message: 'serpente aggiunto'
     })
 };
 //delete 
-function destroy (req,res) {
-    return res.json ({
+function destroy(req, res) {
+    return res.json({
         Message: 'serpente rimosso'
     })
 };
 
-module.exports = {index, show, store, destroy};
+module.exports = { index, show, store, destroy };
