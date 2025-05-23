b93d4b93054f7250ed4fc96e7b0a4e2321dfa592
MegaEmme <mikmusco@gmail.com>
2025-05-23 16:08:22 +0200

    feat: add show and destroy methods to snakeController and update snakesRouter to include corresponding routes

--- controllers/snakeController.js
+++ controllers/snakeController.js
@@ -1,7 +1,27 @@
+//CRUD
+//index
 function index (req,res) {
     return res.json({
         Message: 'sei su index'
     })
-} 
+};
+//show
+function show (req,res) {
+    return res.json({
+        Message: 'sei su show'
+    })
+};
+//store
+function store (req,res) {
+    return res.json ({ 
+        Message: 'serpente aggiunto'
+    })
+};
+//delete 
+function destroy (req,res) {
+    return res.json ({
+        Message: 'serpente rimosso'
+    })
+};
 
-module.exports = {index};
+module.exports = {index, show, store, destroy};
